d1c991ecce8ad4d6e23584984ebdcff9eadfe75b
Tobias <tohub@bluewin.ch>
2021-04-03 23:22:48 +0200

    skeleton

--- src/components/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIPPSPIEL_CONFIG_H
-#define TIPPSPIEL_CONFIG_H
-
-#include <string>
-
-std::string get_config(const std::string&);
-
-#endif
--- src/components/time.cpp
+++
@@ -1,6 +0,0 @@
-#include "time.h"
-
-int get_time()
-{
-    return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-}
